754ec1c20ff2438b628111330c1ca7f28fae020d
Tho'atur Rijal <147828732+Netral2003@users.noreply.github.com>
2023-10-31 16:33:21 +0700

    Update perangkingan.php

--- perangkingan.php
+++ perangkingan.php
@@ -94,5 +94,35 @@ if (isset($_POST['proses'])) {
             <th width="100px">n_Preferensi</th>
         </tr>
 </thead>
- <tbody>
-                
+<tbody>
+    
+<?php
+        $i=1;
+            $sql = "SELECT perangkingan.idperangkingan,pendaftaran.iddaftar,pendaftaran.tgldaftar,pendaftaran.nim,
+                            mahasiswa.nama_mahasiswa,perangkingan.n_pendapatan,perangkingan.n_ipk,
+                            perangkingan.n_saudara,perangkingan.preferensi
+            FROM perangkingan INNER JOIN pendaftaran ON perangkingan.iddaftar=pendaftaran.iddaftar 
+            INNER JOIN mahasiswa ON pendaftaran.nim=mahasiswa.nim ORDER BY preferensi DESC";
+            $result = $conn->query($sql);
+            while($row = $result->fetch_assoc()) {
+        ?>
+        <tr>
+            <td><?php echo $i++; ?></td>
+            <td><?php echo $row['nim']; ?></td>
+            <td><?php echo $row['nama_mahasiswa']; ?></td>
+            <td><?php echo $row['n_pendapatan']; ?></td>
+            <td><?php echo $row['n_ipk']; ?></td>
+            <td><?php echo $row['n_saudara']; ?></td>
+            <td><?php echo $row['preferensi']; ?></td>
+
+        </tr>
+        <?php
+            }
+            $conn->close();
+        ?>
+
+
+    </tbody>
+    </table>
+    </div>
+    </div>               
