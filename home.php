821639496a822d7f604a880ffa95be8961c40773
Tho'atur Rijal <147828732+Netral2003@users.noreply.github.com>
2023-11-20 22:57:25 +0700

    Update home.php

--- home.php
+++ home.php
@@ -69,10 +69,14 @@ require "config.php";
         }
     ?>
 </div>
-    <div class="diagram-container">
-        <img src="diagram.png" hight="600" width="600" alt="diagram"  style="margin-right: 0px; margin-left: 50%; ">
-            </div>
-
+    <ul><div class="kotak ml-5 mt-3" style="width:320px;height:400px:background:;float:left;">
+    <div class="gambar" style="width:100%;height:60%%background:yellow:">
+        <img src="photo1.jpg" style="width:100%;height:100%;">
+    </div>
+   
+   <div class="bg12">
+                 <img src="bg12.jpeg" hight="1000px" width="1000px" alt="diagram"  style="margin-right: 5%; margin-left: 240%; margin-top: -75%; margin-bottom: 10%; width: 100%;">
+            </div></ul>
     <script src="assets/js/jquery-3.7.0.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/all.js"></script>
