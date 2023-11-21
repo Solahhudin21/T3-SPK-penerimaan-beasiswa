814d0100929fa43277abae45eed6eb0086fab9fb
Solahhudin <145647757+Solahhudin21@users.noreply.github.com>
2023-11-21 09:50:14 +0700

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,3 +1,16 @@
 <h1 align="center">SELAMAT DATANG PESERTA SELEKSI BEASISWA</h1>
 
 <h3 align="center">STMIK ELRAHMA YOGYAKARTA</h3>
+
+<div class="diagram-container">
+        <img src="diagram.png" hight="600" width="600" alt="diagram"  style="margin-right: 0px; margin-left: 50%; ">
+            </div>
+
+    <ul><div class="kotak ml-5 mt-3" style="width:320px;height:400px:background:;float:left;">
+    <div class="gambar" style="width:100%;height:60%%background:yellow:">
+        <img src="photo1.jpg" style="width:100%;height:100%;">
+    </div>
+
+   <div class="bg12">
+                 <img src="bg12.jpeg" hight="1000px" width="1000px" alt="diagram"  style="margin-right: 5%; margin-left: 240%; margin-top: -75%; margin-bottom: 10%; width: 100%;">
+            </div></ul>
