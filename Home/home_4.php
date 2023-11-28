1d1034eb2435bb0865b58708d184b6b94d95ccfe
zainalwaeee <96877300+zainalwaeee@users.noreply.github.com>
2023-11-28 08:00:31 +0700

    Add files via upload

---
+++ Home/home_4.php
@@ -0,0 +1,19 @@
+<script src="assets/js/jquery-3.7.0.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/all.js"></script>
+    <script src="assets/js/datatables.min.js"></script>
+    <script>
+       $(document).ready(function () {
+           $('#myTable').dataTable();
+       });
+    </script>
+
+    <script src="assets/js/chosen.jquery.min.js"></script>
+    <script>
+     $(function() {
+       $('.chosen').chosen();
+     });
+    </script>
+
+</body>
+</html>
