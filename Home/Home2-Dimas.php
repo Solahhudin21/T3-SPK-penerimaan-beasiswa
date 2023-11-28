59029c72f660394d468c8e6c8b190804ca49e499
Dimas Agung Prabowo <96804772+dimasagp1@users.noreply.github.com>
2023-11-28 07:53:04 +0700

    Add files via upload

---
+++ Home/Home2-Dimas.php
@@ -0,0 +1,15 @@
+<body>
+
+<!-- tAMPILAN MENU-->
+<nav class="navbar navbar-dark bg-primary border navbar-expand-sm fixed-top">
+    <a class="navbar-brand" href="#">SELEKSI BEASISWA</a>
+    <ul class="navbar-nav">
+        <li class="nav-item active"><a class="nav-link" href="home.php"><i class="fas fa-home"></i> Home </a></li>
+        <li class="nav-item active"><a class="nav-link" href="?page=mahasiswa"><i class="fas fa-user-circle"></i> Mahasiswa </a></li>
+        <li class="nav-item active"><a class="nav-link" href="?page=pendaftaran"><i class="fas fa-address-book"></i> Pendaftaran </a></li>
+        <li class="nav-item active"><a class="nav-link" href="?page=perangkingan&thn"><i class="fas fa-address-book"></i> perangkingan </a></li>
+        <li class="nav-item active"><a class="nav-link" href="index.php"><i class="fas fa-door-closed"></i> logout </a></li>
+    </ul>
+</nav>
+
+<div class="container" style="margin-top:100px;margin-bottom:100px">
