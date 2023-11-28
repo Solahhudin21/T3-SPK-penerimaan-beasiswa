7544d585e8b7e0feb42b666f5dc0c145800c2b69
Tho'atur Rijal <147828732+Netral2003@users.noreply.github.com>
2023-11-28 07:59:50 +0700

    Add files via upload

---
+++ Home/Home1-Rijal.php
@@ -0,0 +1,18 @@
+<?php
+date_default_timezone_set("Asia/Jakarta");
+require "config.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BEASISWA SPK</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/datatables.min.css">
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-chosen.css">
+</head>
